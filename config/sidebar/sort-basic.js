fcb81188ca841829b647d591d119b96618f400d2
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-12-08 00:24:01 +0800

    修改了项目结构。

--- config/sidebar/sort-basic.js
+++ config/sidebar/sort-basic.js
@@ -1,9 +1,9 @@
 module.exports = [
-	"/algorithm-basic/sort/basic/selection.md",
-	"/algorithm-basic/sort/basic/bubble.md",
-	"/algorithm-basic/sort/basic/bubble-optimization.md",
-	"/algorithm-basic/sort/basic/insertion.md",
-	"/algorithm-basic/sort/basic/insertion-meaning.md",
-	"/algorithm-basic/sort/basic/insertion-optimization.md",
-	"/algorithm-basic/sort/basic/shell.md"
+	"/algorithm-basic/basic/selection.md",
+	"/algorithm-basic/basic/bubble.md",
+	"/algorithm-basic/basic/bubble-optimization.md",
+	"/algorithm-basic/basic/insertion.md",
+	"/algorithm-basic/basic/insertion-meaning.md",
+	"/algorithm-basic/basic/insertion-optimization.md",
+	"/algorithm-basic/basic/shell.md"
 ]
--- docs/algorithm-basic/README.md
+++ docs/algorithm-basic/README.md
@@ -1,1 +1,20 @@
 # 第 1 部分 算法基础
+
+- [第 0 章 参考资料](/algorithm-basic/reference/reference.md)
+- [第 1 章 递归与时间复杂度](/algorithm-basic/recursion-time-complexity/intro.md)
+- [第 2 章 循环不变量](/algorithm-basic/loop-invariant/intro.md)
+- [第 3 章 基础排序算法](/algorithm-basic/basic/intro.md)
+- [第 4 章 归并排序](/algorithm-basic/merge-sort/intro.md)
+- [第 5 章 快速排序](/algorithm-basic/quick-sort/intro.md)
+- [第 6 章 非比较排序算法](/algorithm-basic/non-comparison/intro.md)
+- [第 7 章 二分查找](/algorithm-basic/binary-search/1-decrease-and-conquer.md)
+- [第 8 章 滑动窗口](/algorithm-basic/sliding-window/intro.md)
+- [第 9 章 双指针](/algorithm-basic/two-pointers/intro.md)
+
+
+
+
+
+
+
+
