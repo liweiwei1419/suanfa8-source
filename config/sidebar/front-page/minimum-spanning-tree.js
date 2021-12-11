4d79a5563b78f154c27b0fe1faa6c66e2efb5909
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-12-11 23:00:59 +0800

    把「最小生成树」做了一些格式上的调整。

--- config/sidebar/front-page/minimum-spanning-tree.js
+++ config/sidebar/front-page/minimum-spanning-tree.js
@@ -2,7 +2,7 @@ module.exports = [
 	"/graph/minimum-spanning-tree/intro.md",
 	"/graph/minimum-spanning-tree/application.md",
 	"/graph/minimum-spanning-tree/segmentation-theorem.md",
-	"/graph/minimum-spanning-tree/ruskal.md",
+	"/graph/minimum-spanning-tree/kruskal.md",
 	"/graph/minimum-spanning-tree/lazy-prim.md",
 	"/graph/minimum-spanning-tree/summarize.md"
 ]
---
+++ docs/graph/minimum-spanning-tree/application.md
@@ -0,0 +1,11 @@
+---
+title: 26.2 最小生成树的应用
+icon: yongyan
+category: 图论
+tags:
+  - 最小生成树
+---
+
+布线设计：使得边能够连通所有顶点，并且耗费最少。这部分内容不多，《算法（第 4 版）》上也就说了下面这些内容。
+
+![image-20211211225330595](https://tva1.sinaimg.cn/large/008i3skNgy1gxaa8l09l4j31940jigpu.jpg)
