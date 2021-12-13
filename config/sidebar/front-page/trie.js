9b5543213f880aaca679c9b628373a30026d0bc7
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-12-13 12:59:57 +0800

    重新编写了「字典树」部分的 5 节内容。

--- config/sidebar/front-page/trie.js
+++ config/sidebar/front-page/trie.js
@@ -1,4 +1,7 @@
 module.exports = [
-	"/data-structure-advance/trie/intro.md",
-	"/data-structure-advance/trie/practice.md"
+	"/data-structure-advance/trie/01.md",
+	"/data-structure-advance/trie/02.md",
+	"/data-structure-advance/trie/03.md",
+	"/data-structure-advance/trie/04.md",
+	"/data-structure-advance/trie/05.md"
 ]
