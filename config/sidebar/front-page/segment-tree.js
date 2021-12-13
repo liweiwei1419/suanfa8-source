378e0e1826cc6200406ec1f9037d4267072aea3b
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-12-13 13:21:31 +0800

    补充了「线段树」部分的 4 节内容，但是格式还未做修改。

--- config/sidebar/front-page/segment-tree.js
+++ config/sidebar/front-page/segment-tree.js
@@ -1,3 +1,6 @@
 module.exports = [
-	"/graph/dijkstra/intro.md"
+	"/data-structure-advance/segment-tree/01.md",
+	"/data-structure-advance/segment-tree/02.md",
+	"/data-structure-advance/segment-tree/03.md",
+	"/data-structure-advance/segment-tree/04.md"
 ]
