af6798debbeb6e298ed42b9b21adce1a5421e339
liweiwei1419 <121088825@qq.com>
2021-12-26 16:08:30 +0800

    删去了导致部署失败的配置。

--- config/sidebar/choice/choice.js
+++ config/sidebar/choice/choice.js
@@ -7,13 +7,13 @@ module.exports = [
 			
 		]
 	},
-	{
-		"title":"滑动窗口",
-		"icon": "jingxuan",
-		"children": [
+	// {
+	// 	"title":"滑动窗口",
+	// 	"icon": "jingxuan",
+	// 	"children": [
 			
-		]
-	},
+	// 	]
+	// },
 	// {
 	// 	"title":"双指针",
 	// 	"icon": "jingxuan",
@@ -22,15 +22,15 @@ module.exports = [
 	// 		"/choice/binary-search/16.md"
 	// 	]
 	// },
-	{
-		"title":"栈与单调栈",
-		"icon": "jingxuan",
-		"children": [
-			"/choice/stack/0084-largest-rectangle-in-histogram.md",
-			"/choice/stack/0316-remove-duplicate-letters.md",
-			"/choice/stack/1673-find-the-most-competitive-subsequence.md"
-		]
-	},
+	// {
+	// 	"title":"栈与单调栈",
+	// 	"icon": "jingxuan",
+	// 	"children": [
+	// 		"/choice/stack/0084-largest-rectangle-in-histogram.md",
+	// 		"/choice/stack/0316-remove-duplicate-letters.md",
+	// 		"/choice/stack/1673-find-the-most-competitive-subsequence.md"
+	// 	]
+	// },
 	// {
 	// 	"title":"二叉树",
 	// 	"icon": "jingxuan",
@@ -54,18 +54,18 @@ module.exports = [
 	// 		"/choice/binary-search/0053.md"
 	// 	]
 	// },
-	{
-		"title":"拓扑排序",
-		"icon": "jingxuan",
-		"children": [
-			"/choice/topological-sort/intro.md",
-			"/choice/topological-sort/0207-course-schedule.md",
-			"/choice/topological-sort/0210-course-schedule-ii.md",
-			"/choice/topological-sort/0802-find-eventual-safe-states.md",
-			"/choice/topological-sort/1203-sort-items-by-groups-respecting-dependencies.md",
-			// "/choice/topological-sort/0851-loud-and-rich.md",
-		]
-	},
+	// {
+	// 	"title":"拓扑排序",
+	// 	"icon": "jingxuan",
+	// 	"children": [
+	// 		"/choice/topological-sort/intro.md",
+	// 		"/choice/topological-sort/0207-course-schedule.md",
+	// 		"/choice/topological-sort/0210-course-schedule-ii.md",
+	// 		"/choice/topological-sort/0802-find-eventual-safe-states.md",
+	// 		"/choice/topological-sort/1203-sort-items-by-groups-respecting-dependencies.md",
+	// 		// "/choice/topological-sort/0851-loud-and-rich.md",
+	// 	]
+	// },
 	
 	// {
 	// 	"title":"递归",
