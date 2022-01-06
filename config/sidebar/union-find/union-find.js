cb35a97dbc655c93bd0a0eb1d048eceda26d2735
liweiwei1419 <121088825@qq.com>
2022-01-06 15:57:45 +0800

    修改了「并查集」的格式。

--- config/sidebar/union-find/union-find.js
+++ config/sidebar/union-find/union-find.js
@@ -19,11 +19,10 @@ module.exports = [
 		"icon": "shu",
 		"children": [
 			"/union-find/practice.md",
-			"/union-find/solutions/0990-satisfiability-of-equality-equations.md",
-			"/union-find/solutions/0399-evaluate-division.md",
 			"/union-find/solutions/0128-longest-consecutive-sequence.md",
 			"/union-find/solutions/0130-surrounded-regions.md",
 			"/union-find/solutions/0200-number-of-islands.md",
+			"/union-find/solutions/0399-evaluate-division.md",
 			"/union-find/solutions/0547-friend-circles.md",
 			"/union-find/solutions/0684-redundant-connection.md",
 			"/union-find/solutions/0685-redundant-connection-ii.md",
@@ -31,10 +30,11 @@ module.exports = [
 			"/union-find/solutions/0765-couples-holding-hands.md",
 			"/union-find/solutions/0945-minimum-increment-to-make-array-unique.md",
 			"/union-find/solutions/0952-largest-component-size-by-common-factor.md",
+			"/union-find/solutions/0956-minimum-increment-to-make-array-unique.md",
+			"/union-find/solutions/0990-satisfiability-of-equality-equations.md",
 			"/union-find/solutions/1267-count-servers-that-communicate.md",
 			"/union-find/solutions/1319-number-of-operations-to-make-network-connected.md",
 			"/union-find/solutions/1559-detect-cycles-in-2d-grid.md"
 		]
 	}
-	
 ]
