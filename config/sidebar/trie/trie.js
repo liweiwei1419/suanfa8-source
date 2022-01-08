efa4b37a6d72abe3f32d96dd577c1cc556e93923
liweiwei1419 <121088825@qq.com>
2022-01-09 04:23:43 +0800

    删除了「前缀树」的一些问题。

--- config/sidebar/trie/trie.js
+++ config/sidebar/trie/trie.js
@@ -5,12 +5,12 @@ module.exports = [
 		"children": [
 			"/trie/solutions/0208-implement-trie-prefix-tree.md",
 			"/trie/solutions/0211-design-add-and-search-words-data-structure.md",
-			"/trie/solutions/0212.word-search-ii.md",
-			"/trie/solutions/0336-palindrome-pairs.md",
+			// "/trie/solutions/0212.word-search-ii.md",
+			// "/trie/solutions/0336-palindrome-pairs.md",
 			"/trie/solutions/0745-prefix-and-suffix-search.md",
-			"/trie/solutions/0648-replace-words.md",
+			// "/trie/solutions/0648-replace-words.md",
 			"/trie/solutions/0677-map-sum-pairs.md",
-			"/trie/solutions/0720-longest-word-in-dictionary.md"
+			// "/trie/solutions/0720-longest-word-in-dictionary.md"
 		]
 	}
 ]
