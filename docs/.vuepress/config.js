d62d08514232734b1c9fd50191378eb25f703501
liweiwei1419 <121088825@qq.com>
2022-05-02 13:24:04 +0800

    更新了配置。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,41 +1,37 @@
 const navConfig = require('../../config/navConfig.js');
 const sidebarConfig = require('../../config/sidebarConfig.js');
 
-const { config } = require("vuepress-theme-hope");
+const { config } = require('vuepress-theme-hope');
 
 module.exports = config({
-  title: "算法吧",
-  description: "欢迎来到算法吧",
+  title: '算法吧',
+  description: '欢迎来到算法吧',
   // dest: "./dist",
 
   head: [
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js' }],
     [
-      "script",
-      { src: "https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" },
-    ],
-    [
-      "script",
+      'script',
       {
-        src: "https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js",
+        src: 'https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js',
       },
     ],
-    ["script", { src: "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js" }],
-    [
-      "script",
-      { src: "https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js" },
-    ],
-
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js' }],
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js' }],
 
     [
-      "script", {
-        "data-ad-client": "ca-pub-6066632930480255",
+      'script',
+      {
+        'data-ad-client': 'ca-pub-6066632930480255',
         async: true,
-        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      }
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+      },
     ],
 
     [
-    "script", {}, `
+      'script',
+      {},
+      `
     var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
@@ -43,17 +39,17 @@ module.exports = config({
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-    `
-  ],
-    ['link', { rel: 'icon', href: '/suanfa8.png' }]
+    `,
+    ],
+    ['link', { rel: 'icon', href: '/suanfa8.png' }],
   ],
 
   themeConfig: {
     logo: '/suanfa8.png',
-    hostname: "http://suanfa8.com/",
+    hostname: 'https://suanfa8.com/',
 
-    author: "liweiwei1419",
-    repo: "liweiwei1419/liweiwei1419.github.io",
+    author: 'liweiwei1419',
+    repo: 'liweiwei1419/liweiwei1419.github.io',
 
     nav: navConfig,
     sidebar: sidebarConfig,
@@ -74,23 +70,24 @@ module.exports = config({
 
     footer: {
       display: true,
-      content: "欢迎来到算法吧",
+      content: "<a href='https://beian.miit.gov.cn' target='_blank' >闽ICP备2022001209号</a>",
     },
 
     comment: {
-      type: "valine",
-      appId: "mqMDM2jkdekHlk3GRbuUPjoG-gzGzoHsz", // your appId
-      appKey: "8kmH8Wx6Dh5ewLScMuafVH9o", // your appKey
+      type: 'valine',
+      appId: 'mqMDM2jkdekHlk3GRbuUPjoG-gzGzoHsz', // your appId
+      appKey: '8kmH8Wx6Dh5ewLScMuafVH9o', // your appKey
       // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
-      placeholder: "留言请不要贴一段代码，让别人找 bug，代码问题请自己调试。填写 E-mail 在收到回复的时候会有邮件通知，有反馈和建议请到右上角「留言反馈」处留言，或者给本 GitHub 提一个 issue。"
+      placeholder:
+        '留言请不要贴一段代码，让别人找 bug，代码问题请自己调试。填写 E-mail 在收到回复的时候会有邮件通知，有反馈和建议请到右上角「留言反馈」处留言，或者给本 GitHub 提一个 issue。',
     },
 
     copyright: {
-      status: "global",
+      status: 'global',
     },
 
     git: {
-      timezone: "Asia/Shanghai",
+      timezone: 'Asia/Shanghai',
     },
 
     search: true,
@@ -100,71 +97,71 @@ module.exports = config({
       enableAll: true,
       presentation: {
         plugins: [
-          "highlight",
-          "math",
-          "search",
-          "notes",
-          "zoom",
-          "anything",
-          "audio",
-          "chalkboard",
+          'highlight',
+          'math',
+          'search',
+          'notes',
+          'zoom',
+          'anything',
+          'audio',
+          'chalkboard',
         ],
       },
     },
 
     pwa: {
-      favicon: "/favicon.ico",
+      favicon: '/favicon.ico',
       cachePic: true,
       apple: {
-        icon: "/assets/icon/apple-icon-152.png",
-        statusBarColor: "black",
+        icon: '/assets/icon/apple-icon-152.png',
+        statusBarColor: 'black',
       },
       msTile: {
-        image: "/assets/icon/ms-icon-144.png",
-        color: "#ffffff",
+        image: '/assets/icon/ms-icon-144.png',
+        color: '#ffffff',
       },
       manifest: {
         icons: [
           {
-            src: "/assets/icon/chrome-mask-512.png",
-            sizes: "512x512",
-            purpose: "maskable",
-            type: "image/png",
+            src: '/assets/icon/chrome-mask-512.png',
+            sizes: '512x512',
+            purpose: 'maskable',
+            type: 'image/png',
           },
           {
-            src: "/assets/icon/chrome-mask-192.png",
-            sizes: "192x192",
-            purpose: "maskable",
-            type: "image/png",
+            src: '/assets/icon/chrome-mask-192.png',
+            sizes: '192x192',
+            purpose: 'maskable',
+            type: 'image/png',
           },
           {
-            src: "/assets/icon/chrome-512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: '/assets/icon/chrome-512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
           {
-            src: "/assets/icon/chrome-192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/assets/icon/chrome-192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
         ],
         shortcuts: [
           {
-            name: "Guide",
-            short_name: "Guide",
-            url: "/guide/",
+            name: 'Guide',
+            short_name: 'Guide',
+            url: '/guide/',
             icons: [
               {
-                src: "/assets/icon/guide-maskable.png",
-                sizes: "192x192",
-                purpose: "maskable",
-                type: "image/png",
+                src: '/assets/icon/guide-maskable.png',
+                sizes: '192x192',
+                purpose: 'maskable',
+                type: 'image/png',
               },
               {
-                src: "/assets/icon/guide-monochrome.png",
-                sizes: "192x192",
-                purpose: "monochrome",
-                type: "image/png",
+                src: '/assets/icon/guide-monochrome.png',
+                sizes: '192x192',
+                purpose: 'monochrome',
+                type: 'image/png',
               },
             ],
           },
@@ -178,7 +175,6 @@ module.exports = config({
     //   type: "vssue",
     //   // 设置 `platform` 而不是 `api`
     //   platform: "github",
-
     //   // 其他的 Vssue 配置
     //   owner: "suanfa8",
     //   repo: "suanfa8.github.io",
